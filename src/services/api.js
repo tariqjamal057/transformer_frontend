b29d03294372cafbc029d7eecee767df80fc6d36
Tariq Jamal A <tariqjamal4267324@gmail.com>
2026-01-10 20:57:44 +0530

    resolve api error

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // Add a request interceptor to include the token in the headers
